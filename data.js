849d940ecdd160ea315e9e93969bd614932fe0b7
we0are0back0-max <we0are0back0@gmail.com>
2026-08-22 01:53:24 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const myPlaylist = [
+    {
+        name: "Do Lafzo Mein",
+        audio: "https://cdn.jsdelivr.net/gh/we0are0back0-max/trucker-music-web@main/_Do%20Lafzo%20Mein%20%5BFull%20Song%5D_%20Dhaai%20Akshar%20Prem%20Ke%20Ft%20Aishwarya%20Rai_%20Abhishek%20Bacchan%28MP3_160K%29.mp3",
+        cover: "https://cdn.jsdelivr.net/gh/we0are0back0-max/trucker-music-web@main/64e285ad54fd721bc64723753da45c98.jpg"
+    },
+    {
+        name: "Dil Ke Badle Sanam",
+        audio: "https://cdn.jsdelivr.net/gh/we0are0back0-max/trucker-music-web@main/Dil%20Ke%20Badle%20Sanam%20%28Full%20Song%29%20Film%20-%20Kyon%20Ki%20...It_S%20Fate%28MP3_160K%29.mp3",
+        cover: "https://cdn.jsdelivr.net/gh/we0are0back0-max/trucker-music-web@main/64e285ad54fd721bc64723753da45c98.jpg"
+    }
+];
