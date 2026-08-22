532ab5d4a5eacd588e3859fa416573cd6705f319
we0are0back0-max <we0are0back0@gmail.com>
2026-08-22 10:48:28 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const myPlaylist = [
     { name: "Oye Raju Payar Na Kariyo", artist: "Artist Name", audio: "song3.mp3", cover: "song3.jpg" },
     { name: "Kuon Ki Itna Pyar", artist: "Artist Name", audio: "song4.mp3", cover: "song4.jpg" },
     { name: "Do Lafzon Me", artist: "Artist Name", audio: "song5.mp3", cover: "song5.jpg" },
-    { name: "....name ....", artist: "Artist Name", audio: "song6.mp3", cover: "song6.jpg" },
+    { name: "Kyo kisi ko ( tere nam ) ", artist: "Artist Name", audio: "song6.mp3", cover: "song6.jpg" },
     { name: "....name ....", artist: "Artist Name", audio: "song7.mp3", cover: "song7.jpg" },
     { name: "....name ....", artist: "Artist Name", audio: "song8.mp3", cover: "song8.jpg" },
     { name: "....name ....", artist: "Artist Name", audio: "song9.mp3", cover: "song9.jpg" },
