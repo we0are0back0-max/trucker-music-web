59f292e40684ec8f10c909c9da27e4a142f95147
we0are0back0-max <we0are0back0@gmail.com>
2026-08-25 13:36:27 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,4 +1,3 @@
 const ytPlaylist = [
-    { name: "YouTube Truck Song 1", artist: "YT Artist 1", ytId: "https://youtu.be/k4QJovSsEWU?si=u228hmtoKeXbZ_Fu" },
-    { name: "YouTube Truck Song 2", artist: "YT Artist 2", ytId: "https://youtu.be/k4QJovSsEWU?si=u228hmtoKeXbZ_Fu" }
+    { name: "Chaaha Hai Tujhko (Mann)", artist: "Udit Narayan & Anuradha Paudwal", ytId: "k4QJovSsEWU" }
 ];
