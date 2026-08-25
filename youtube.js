3895568c21b62afc7c8be46bb6302083ae6ab232
we0are0back0-max <we0are0back0@gmail.com>
2026-08-25 16:24:47 +0530

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,12 @@
+const ytPlaylist = [
+    { name: "Chaaha Hai Tujhko", artist: "Udit Narayan & Anuradha Paudwal", ytId: "k4QJovSsEWU" },
+    { name: "Jo Bhi Kasmein", artist: "Alka Yagnik & Udit Narayan", ytId: "B72_HsUR0Vc" },
+    { name: "O O Jaane Jaana", artist: "Kamaal Khan", ytId: "vJ_k3O24X24" },
+    { name: "Chand Sifarish", artist: "Shaan & Kailash Kher", ytId: "zWEOx7TS53U" },
+    { name: "Bole Chudiyan", artist: "Sonu Nigam & Alka Yagnik", ytId: "IBvg3WeqP1U" },
+    { name: "Aaye Ho Meri Zindagi Mein", artist: "Udit Narayan", ytId: "c2a_x3M8q0g" },
+    { name: "Chura Ke Dil Mera", artist: "Kumar Sanu & Alka Yagnik", ytId: "8aX_P_q5L40" },
+    { name: "Aksar Is Duniya Mein", artist: "Alka Yagnik", ytId: "yW0_mZ1S_eA" },
+    { name: "Pardesi Pardesi Mere", artist: "Udit Narayan & Alka Yagnik", ytId: "3S1N_p4sR80" },
+    { name: "Shikwa Nahi Kisi Se", artist: "Kumar Sanu", ytId: "Q3X2g91z1gM" }
+];
