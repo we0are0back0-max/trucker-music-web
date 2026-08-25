d8c39bd6067d4f51c817b11b6e869283dafb326f
we0are0back0-max <we0are0back0@gmail.com>
2026-08-25 13:42:40 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,3 +1,12 @@
 const ytPlaylist = [
-    { name: "Chaaha Hai Tujhko (Mann)", artist: "Udit Narayan & Anuradha Paudwal", ytId: "k4QJovSsEWU" }
+    { 
+        name: "Chaaha Hai Tujhko (Mann)", 
+        artist: "Udit Narayan & Anuradha Paudwal", 
+        ytId: "k4QJovSsEWU" 
+    },
+    { 
+        name: "Jo Bhi Kasmein (Raaz)", 
+        artist: "Alka Yagnik & Udit Narayan", 
+        ytId: "B72_HsUR0Vc" 
+    }
 ];
