89e64584f49e11de4522af975ebc49e222c1e5a6
we0are0back0-max <we0are0back0@gmail.com>
2026-08-25 13:31:49 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,4 +1,4 @@
 const ytPlaylist = [
-    { name: "YouTube Truck Song 1", artist: "YT Artist 1", ytId: "dQw4w9WgXcQ" },
-    { name: "YouTube Truck Song 2", artist: "YT Artist 2", ytId: "3JZ_D3ELwOQ" }
+    { name: "YouTube Truck Song 1", artist: "YT Artist 1", ytId: "https://youtu.be/k4QJovSsEWU?si=u228hmtoKeXbZ_Fu" },
+    { name: "YouTube Truck Song 2", artist: "YT Artist 2", ytId: "https://youtu.be/k4QJovSsEWU?si=u228hmtoKeXbZ_Fu" }
 ];
