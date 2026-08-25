142af2ece9a1357ee9746d1e909830055706b0c4
we0are0back0-max <we0are0back0@gmail.com>
2026-08-25 13:28:52 +0530

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,4 @@
+const ytPlaylist = [
+    { name: "YouTube Truck Song 1", artist: "YT Artist 1", ytId: "dQw4w9WgXcQ" },
+    { name: "YouTube Truck Song 2", artist: "YT Artist 2", ytId: "3JZ_D3ELwOQ" }
+];
